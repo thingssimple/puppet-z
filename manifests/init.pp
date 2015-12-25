fb3042d8a4b1f695751dd469fd6d900a54987de5
Michael Avila <me@michaelavila.com>
2015-12-24 23:44:17 -0800

    Remove incorrect file declaration

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,10 +5,6 @@ class z {
 
   $home = "/Users/${::boxen_user}"
 
-  file { "${home}/.zshrc":
-    ensure => present
-  }
-
   file_line { "Add gnubin to all environment man paths":
     path => "${home}/.zshrc",
     line => 'source `brew --prefix`/etc/profile.d/z.sh'
