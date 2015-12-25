db8a1bcae4f9df1cdd707ad6cf71f58732350e54
Michael Avila <me@michaelavila.com>
2015-12-24 23:42:33 -0800

    Configure z

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,15 @@ class z {
   package { 'z':
     ensure => present
   }
+
+  $home = "/Users/${::boxen_user}"
+
+  file { "${home}/.zshrc":
+    ensure => present
+  }
+
+  file_line { "Add gnubin to all environment man paths":
+    path => "${home}/.zshrc",
+    line => 'source `brew --prefix`/etc/profile.d/z.sh'
+  }
 }
